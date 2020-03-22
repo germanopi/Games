bc4c5c6a54d94e3112cf1003015eb3213a14c26a
unknown <gpc2@cin.ufpe.br>
2020-03-21 23:21:26 -0300

    Corrida

---
+++ Unity/Corrida/Assets/AI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    private float dir=0;
+    private float speed=0.5f;
+    private bool tocou=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject boxRight=GameObject.FindGameObjectWithTag("right");
+        if(Vector3.Distance(transform.position,boxRight.transform.position)<10f){
+            dir=90;
+        }
+         GameObject boxUp=GameObject.FindGameObjectWithTag("up");
+        if(Vector3.Distance(transform.position,boxUp.transform.position)<10f){
+            dir=0;
+        }
+         GameObject boxLeft=GameObject.FindGameObjectWithTag("left");
+        if(Vector3.Distance(transform.position,boxLeft.transform.position)<10f){
+            dir=-90;
+        }
+        Rigidbody rigidBody=GetComponent<Rigidbody>();
+        if(dir==0){
+            if(rigidBody.velocity.magnitude<9f){
+                rigidBody.AddForce(Vector3.forward*speed,ForceMode.Impulse);
+            }
+        }
+        if(dir==90){
+            if(rigidBody.velocity.magnitude<9f){
+                rigidBody.AddForce(Vector3.right*speed,ForceMode.Impulse);
+            }
+        } 
+        if(dir==-90){
+            if(rigidBody.velocity.magnitude<9f){
+                rigidBody.AddForce(Vector3.left*speed,ForceMode.Impulse);
+            }
+        }
+        transform.rotation=Quaternion.Lerp(transform.rotation,Quaternion.Euler(0,dir,0),Time.deltaTime*2f);
+    }
+}
---
+++ Unity/Corrida/Assets/Movimento.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    private float dir=0;
+    private float speed=0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidBody=GetComponent<Rigidbody>();
+        if(Input.GetKey("up")){
+            dir=0;
+            if(rigidBody.velocity.magnitude<9f){
+                rigidBody.AddForce(Vector3.forward*speed,ForceMode.Impulse);
+            }
+        }
+        if(Input.GetKey("right")){
+            dir=90;
+            if(rigidBody.velocity.magnitude<9f){
+                rigidBody.AddForce(Vector3.right*speed,ForceMode.Impulse);
+            }
+        } 
+        if(Input.GetKey("left")){
+            dir=-90;
+            if(rigidBody.velocity.magnitude<9f){
+                rigidBody.AddForce(Vector3.left*speed,ForceMode.Impulse);
+            }
+        }
+        if(Input.GetKey("down")){
+            dir=0;
+            if(rigidBody.velocity.magnitude<9f){
+                rigidBody.AddForce(-Vector3.forward*speed,ForceMode.Impulse);
+            }
+        }
+        transform.rotation=Quaternion.Lerp(transform.rotation,Quaternion.Euler(0,dir,0),Time.deltaTime*2f);
+    }
+}
